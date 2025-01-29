184a0d1a4120508af04a7c7e15cd34e317f0ea9c
ether <3ther.not.found@gmail.com>
2025-01-29 10:17:57 +0000

    Start of project

---
+++ src/test/java/snagicky/collector/SnagickCollectorApplicationTests.java
@@ -0,0 +1,13 @@
+package snagicky.collector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnagickCollectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
