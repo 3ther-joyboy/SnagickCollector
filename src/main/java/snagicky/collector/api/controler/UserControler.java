0f166ab17980dd71de82fa524c42e73284146b30
_3tHeR_ <3ther.not.found@gmail.com>
2025-05-15 00:30:33 +0200

    Fix todo

--- src/main/java/snagicky/collector/api/controler/UserControler.java
+++ src/main/java/snagicky/collector/api/controler/UserControler.java
@@ -17,7 +17,7 @@ import java.util.Set;
 import java.util.UUID;
 
 // For not that messy saving cards
-enum CardAction {
+enum CardAction { // TODO fix this.. this is not working
     save, own;
 }
 
